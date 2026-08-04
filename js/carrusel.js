c539d85826f23ca69ca67e980c0c2797b38bc0fa
lorenzadurann-commits <lorenza.duran.n@gmail.com>
2026-08-04 13:27:31 -0600

    h

--- js/carrusel.js
+++ js/carrusel.js
@@ -51,4 +51,55 @@ document.querySelector(".anterior").onclick=function(){
 
 }; 
 
+// =========================
+// CARRUSEL INDICAS
+// =========================
+
+const slidesIndicas = document.querySelectorAll(".slide-indicas");
+const puntosIndicas = document.querySelectorAll(".punto-indicas");
+
+let indiceIndicas = 0;
+
+
+function mostrarIndicas(n){
+
+    slidesIndicas.forEach(slide=>{
+        slide.classList.remove("activa-indicas");
+    });
+
+    puntosIndicas.forEach(punto=>{
+        punto.classList.remove("activo-indicas");
+    });
+
+    slidesIndicas[n].classList.add("activa-indicas");
+    puntosIndicas[n].classList.add("activo-indicas");
+}
+
+
+document.querySelector(".siguiente-indicas").onclick=function(){
+
+    indiceIndicas++;
+
+    if(indiceIndicas >= slidesIndicas.length){
+        indiceIndicas = 0;
+    }
+
+    mostrarIndicas(indiceIndicas);
+};
+
+
+document.querySelector(".anterior-indicas").onclick=function(){
+
+    indiceIndicas--;
+
+    if(indiceIndicas < 0){
+        indiceIndicas = slidesIndicas.length - 1;
+    }
+
+    mostrarIndicas(indiceIndicas);
+};
+
+
+mostrarIndicas(0);
+
 
