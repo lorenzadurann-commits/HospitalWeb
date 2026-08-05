64a39f5a6cbf2bd41f13813399a683015f27eb3b
lorenzadurann-commits <lorenza.duran.n@gmail.com>
2026-08-05 13:03:57 -0600

    h

--- js/carrusel-indicas.js
+++ js/carrusel-indicas.js
@@ -44,3 +44,69 @@ document.querySelector(".anterior-indicas").onclick=function(){
 
 
 mostrarIndicas(0);
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+    const slidesIndicas =
+        document.querySelectorAll(".slide-indicas");
+
+    const puntosIndicas =
+        document.querySelectorAll(".punto-indicas");
+
+    const anteriorIndicas =
+        document.querySelector(".anterior-indicas");
+
+    const siguienteIndicas =
+        document.querySelector(".siguiente-indicas");
+
+
+    let indiceIndicas = 0;
+
+
+    function mostrarIndicas(indice){
+
+        slidesIndicas.forEach(slide=>{
+            slide.classList.remove("activa-indicas");
+        });
+
+        puntosIndicas.forEach(punto=>{
+            punto.classList.remove("activo-indicas");
+        });
+
+
+        slidesIndicas[indice]
+            .classList.add("activa-indicas");
+
+        puntosIndicas[indice]
+            .classList.add("activo-indicas");
+
+    }
+
+
+    siguienteIndicas.addEventListener("click",()=>{
+
+        indiceIndicas++;
+
+        if(indiceIndicas >= slidesIndicas.length){
+            indiceIndicas = 0;
+        }
+
+        mostrarIndicas(indiceIndicas);
+
+    });
+
+
+    anteriorIndicas.addEventListener("click",()=>{
+
+        indiceIndicas--;
+
+        if(indiceIndicas < 0){
+            indiceIndicas = slidesIndicas.length-1;
+        }
+
+        mostrarIndicas(indiceIndicas);
+
+    });
+
+
+});
